f92858192f778b190b003020d862cb0760e4a5ed
student <reg@relabs.ru>
2022-02-14 02:42:55 +0600

    restructure app for heroku

--- auto_chess/settings.py
+++ auto_chess/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'auto_chess.api.apps',
+    'api',
     'rest_framework',
     # 'channels',
 ]
